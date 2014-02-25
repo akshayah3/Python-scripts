a806bf5b8b7cd009480a5c59b0534fc34faf607f
akshayah3 <akshaynukala95@gmail.com>
2014-02-25 19:39:07 +0530

    Script

--- Gsoc_organisations/gsoc.py
+++ Gsoc_organisations/gsoc.py
@@ -1,1 +1,9 @@
 __author__ = 'Akshay'
+from numpy import*
+
+gsoc_14 = loadtxt('organisations.txt', dtype = 'string')  # Get all the organisations of 2014
+gsoc_13 = loadtxt('organisations1.txt', dtype= 'string')  # Get all the organisations of 2013
+c = [i.split(',')[1] for i in gsoc_14]
+d = [i.split(',')[1] for i in gsoc_13]
+e = [i for i in c if i not in d]
+print e  # Array containing new organisations when compared to 2013
